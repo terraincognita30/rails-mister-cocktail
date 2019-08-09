0ebd15ef54d059fbac7f5a9f3e5deb0b0243f792
Carmen Chung <jlchung416@gmail.com>
2019-08-09 13:11:27 +0900

    what

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # require 'open-uri'
 # require 'JSON'
 
+Ingredient.destroy_all
+Review.destroy_all
+
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 json = open(url).read
 ingredients_stuff = JSON.parse(json)
@@ -9,9 +12,6 @@ ingredients_stuff['drinks'].each do |ingredient|
 end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
-Ingredient.destroy_all
-Review.destroy_all
 #
 # Examples:
 #

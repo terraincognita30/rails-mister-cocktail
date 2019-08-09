bd81f2874cc84ce5a1cf12a2ee1d23a96715d276
Carmen Chung <jlchung416@gmail.com>
2019-08-09 13:51:17 +0900

    what

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,28 +26,27 @@ Ingredient.create(name: "red")
 Ingredient.create(name: "green")
 Ingredient.create(name: "Absolut Vodka")
 
-wheresdoug = Cocktail.create(name: "Where's Doug", picture_url: "bg2.jpg")
+wheresdoug = Cocktail.create(name: "Where's Doug")
 Dose.create(cocktail: wheresdoug, ingredient: Ingredient.find_by(name: 'ginger beard'), description: '1 full')
 Review.create(cocktail: wheresdoug, review_name: 'Kingsley', content: 'Unsettling')
 Review.create(cocktail: wheresdoug, review_name: 'Doug', content: 'Refreshing')
 
-thejordan = Cocktail.create(name: "The Jordan", picture_url: "bg2.jpg")
+thejordan = Cocktail.create(name: "The Jordan")
 Dose.create(cocktail: thejordan, ingredient: Ingredient.find_by(name: 'curry'), description: '1')
 Dose.create(cocktail: thejordan, ingredient: Ingredient.find_by(name: 'naan'), description: '1')
 Review.create(cocktail: thejordan, review_name: 'Kingsley', content: 'This is not a cocktail')
 Review.create(cocktail: thejordan, review_name: 'Anon 2', content: 'Not available on Fridays')
 Review.create(cocktail: thejordan, review_name: 'Anon', content: 'Lakshmi is better')
 
-
-therake = Cocktail.create(name: "The Rake", picture_url: "therake.png")
+therake = Cocktail.create(name: "The Rake")
 Dose.create(cocktail: therake, ingredient: Ingredient.find_by(name: 'green'), description: '1 drop of')
 Dose.create(cocktail: therake, ingredient: Ingredient.find_by(name: 'red'), description: '1 bucket of')
 Review.create(cocktail: therake, review_name: 'Anon', content: 'Too much red, too little green')
 
-yannbreak = Cocktail.create(name: "Yann Break", picture_url: "bg2.jpg")
+yannbreak = Cocktail.create(name: "Yann Break")
 Dose.create(cocktail: yannbreak, ingredient: Ingredient.find_by(name: 'water'), description: '1 drop of')
 
-livecode = Cocktail.create(name: "Livecode", picture_url: "bg2.jpg")
+livecode = Cocktail.create(name: "Livecode")
 Dose.create(cocktail: livecode, ingredient: Ingredient.find_by(name: 'Jägermeister'), description: '1')
 Review.create(cocktail: livecode, review_name: 'Student 1', content: 'Lakshmi is better')
 Review.create(cocktail: livecode, review_name: 'Student 2', content: 'Beer in fridge tastes better')
